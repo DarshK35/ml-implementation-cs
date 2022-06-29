ea2a65ee0e647954f0eec4677f785a669f665b20
Darsh Kumar <darsh.kumar2002@gmail.com>
2022-06-29 14:50:46 +0530

    Cleaned up Matrix base

--- ML Implementations in CSharp/Matrix.cs
+++ ML Implementations in CSharp/Matrix.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-internal class Matrix {
+﻿internal class Matrix {
 	float[,] data;
 	int rows, cols;
 
