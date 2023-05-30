04f3d41d26dc2b8d752f4295d44f1244b96718fb
Darsh Kumar <darsh.kumar2002@gmail.com>
2023-05-31 00:52:54 +0530

    Added Feed Forward Functionality

--- NeuralNetwork.cs
+++ NeuralNetwork.cs
@@ -23,7 +23,23 @@ public class NeuralNetwork {
 		}
 	}
 
-	private void FeedForward(Matrix data) {
-		
+	private Matrix FeedForward(Matrix data) {
+		Matrix layerOutput = ActivationFunction(data.copy() + bias[0]);
+
+		for(int i = 1; i < layerSize.Length; i++) {
+			layerOutput = layerOutput * weights[i - 1];
+			layerOutput = ActivationFunction(layerOutput * bias[i]);
+		}
+		return layherOutput;
+	}
+
+	private Matrix ActivationFunction(Matrix data) {
+		Matrix ret = data.copy();
+		for(int i = 0; i < data.rows; i++) {
+			for(int j = 0; j < data.cols; j++) {
+				ret[i, j] = Math.Tanh(data[i, j]);
+			}
+		}
+		return ret;
 	}
 }
